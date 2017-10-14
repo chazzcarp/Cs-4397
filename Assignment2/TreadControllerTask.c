f0376ccef82c226feb47c1e61f220653276b37ff
chazzcarp <chazzcarpenter@gmail.com>
2017-10-14 18:33:10 -0500

    Update TreadControllerTask.c

--- Assignment2/TreadControllerTask.c
+++ Assignment2/TreadControllerTask.c
@@ -2,6 +2,7 @@
 #include "TreadControllerTask.h"
 #include "MovementDirection.h"
 
+// Wait one-fifth of a second before turning to let the robot move closer to the center of the path.
 void ExecuteTreadControllerTask(UInt8 movement_direction)
 {
 	if((movement_direction == ROTATE_LEFT))
